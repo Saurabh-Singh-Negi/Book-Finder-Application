eef1ffe7962585722df3f0dfad2a185e8201b05c
Saurabh <saurabhnegi0007@gmail.com>
2023-02-23 14:16:50 +0530

    Styling changed for Navbar component

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,12 +1,11 @@
 import React from "react";
 import Searchbar from "./Searchbar";
-
 const Navbar = () => {
   return (
     <>
       <nav className="flex flex-row justify-between bg-[#EAEAEC] p-2">
         <div>
-          <h1>BookFinder</h1>
+          <h1 className="font-extrabold text-2xl cursor-pointer">BookFinder</h1>
         </div>
 
         <Searchbar />
